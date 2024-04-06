d9a2279a67d8f788ce31bd92c38fcbfdf2ae682a
Carlos <cabtuncarlos@gmail.com>
2024-04-06 11:58:58 -0600

    Panel del calculo
    Se calculo la ocupación de la máquina fin press

--- capacidadMq/src/app/ocupacionMq/calcular-capacidad/calcular-capacidad.component.ts
+++ capacidadMq/src/app/ocupacionMq/calcular-capacidad/calcular-capacidad.component.ts
@@ -33,6 +33,7 @@ export class CalcularCapacidadComponent {
   tiempoEfecDiario:number=0;
   ensamReqXdia:number=0;
   ensamXdiaMasScrap:number=0;
+  ProyectadoOcupacionAnual:number=0;
 
   verPestania(estado:boolean){
     this.pestania1=estado;
@@ -64,7 +65,7 @@ export class CalcularCapacidadComponent {
     this.HoraSemanaTurno = dato.horasDelTurno * dato.diasAlaborarSemana * dato.turnosAlDia;
     console.log("horas por semana por turno:",this.HoraSemanaTurno);
 
-    let ensamblesPorminuto = dato.cortesXminuto / 46;
+    let ensamblesPorminuto = dato.cortesXminuto / 46; //aqui se va a cambiar los 46, pero eso viene del catalogo
     this.ensamblesXminuto = Number(ensamblesPorminuto.toFixed(3)); // o Number(ensamblesPorminuto.toFixed(3));
     console.log("ensambles por minuto:",this.ensamblesXminuto);
 
@@ -72,19 +73,35 @@ export class CalcularCapacidadComponent {
     this.ensamblesXhora = Number(ensamblesPorhora.toFixed(3));
     console.log("ensambles por hora:",this.ensamblesXhora)
 
-    this.requerimientoSem = dato.requeriAnualAutilizar / dato.semanasAlaborar;
-    console.log("Requerimiento Semanal:",this.requerimientoSem);
+    if (dato.requeriAnualAutilizar && dato.semanasAlaborar) {
+      this.requerimientoSem = dato.requeriAnualAutilizar / dato.semanasAlaborar;
+      console.log("Requerimiento Semanal:",this.requerimientoSem);
+    } else {
+      this.requerimientoSem = 0;
+      console.log("Requerimiento Semanal:",this.requerimientoSem);
+    }
 
-    this.ensamReqXdia = this.requerimientoSem / dato.diasAlaborarSemana;
-    console.log("Ensamble requerido por dia:", this.ensamReqXdia);
+    if (this.requerimientoSem && dato.diasAlaborarSemana) {
+      this.ensamReqXdia = this.requerimientoSem / dato.diasAlaborarSemana;
+      console.log("Ensamble requerido por dia:", this.ensamReqXdia);
+    } else {
+      this.ensamReqXdia = 0;
+      console.log("Ensamble requerido por dia:", this.ensamReqXdia);
+    }
     
-    let ensableMasScrap = this.ensamReqXdia * (1+dato.scrapLiberado);
+    
+    let ensableMasScrap = this.ensamReqXdia * (1+(dato.scrapLiberado/100));
     this.ensamXdiaMasScrap = Number(ensableMasScrap.toFixed(3));
     console.log("Ensamble por dia mas Scrap:",this.ensamXdiaMasScrap);
 
     let horasRequerimientoDiario = this.ensamXdiaMasScrap / this.ensamblesXhora
-    this.hrsNcesRequeDiario = Number(horasRequerimientoDiario.toFixed(3));
-    console.log("Horas necesarias requerimiento diario:", this.hrsNcesRequeDiario);
+    if (horasRequerimientoDiario) {
+      this.hrsNcesRequeDiario = Number(horasRequerimientoDiario.toFixed(3));
+      console.log("Horas necesarias requerimiento diario:", this.hrsNcesRequeDiario);
+    } else {
+      this.hrsNcesRequeDiario = 0;
+      console.log("Horas necesarias requerimiento diario:", this.hrsNcesRequeDiario);
+    }
     
     let primero = dato.demorasInevitables + dato.tiempoMuertoPlan + dato.tiempoSetUp;
     let segundo = dato.horasDelTurno - primero / 60;
@@ -92,7 +109,14 @@ export class CalcularCapacidadComponent {
     this.tiempoEfecDiario = Number(resultado.toFixed(3));
     //(dato.horasDelTurno((dato.demorasInevitables+dato.tiempoMuertoPlan+dato.tiempoSetUp)/60))*dato.turnosAlDia;
     console.log("tiempo efectivo diario:",this.tiempoEfecDiario);
-    
+
+    let primeroPorcentaje = horasRequerimientoDiario / this.tiempoEfecDiario;
+    let segundoPorcentaje = primeroPorcentaje * 100;
+    if (segundoPorcentaje) {
+      this.ProyectadoOcupacionAnual = Number(segundoPorcentaje.toFixed(2));
+    } else {
+      this.ProyectadoOcupacionAnual = 0;
+    }
 
   }
 
