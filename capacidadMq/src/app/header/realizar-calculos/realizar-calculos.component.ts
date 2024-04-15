cb5dceeedf0d88be38dc0d0aae879c9f5c372cac
Carlos <cabtuncarlos@gmail.com>
2024-04-15 17:16:47 -0600

    Menu
    se esta creando el nuevo menu que se utilizara en los catalogo

--- capacidadMq/src/app/header/realizar-calculos/realizar-calculos.component.ts
+++ capacidadMq/src/app/header/realizar-calculos/realizar-calculos.component.ts
@@ -7,4 +7,62 @@ import { Component } from '@angular/core';
 })
 export class RealizarCalculosComponent {
 
+  //variables para ver los distintos formularios
+  datoGeneral:boolean=true;
+  maquinasForm:boolean=false;
+  plantasForm:boolean=false;
+  departamentosForm:boolean=false;
+  parteForm:boolean=false;
+  funcionForm:boolean=false;
+
+  datosGenerales(estado:boolean){
+    this.datoGeneral   = estado;
+    this.maquinasForm = !estado;
+    this.plantasForm = !estado;
+    this.departamentosForm = !estado;
+    this.parteForm = !estado;
+    this.funcionForm = !estado;
+  }
+
+  catalogoMaquinas(estado:boolean){
+    this.datoGeneral   = !estado;
+    this.maquinasForm = estado;
+    this.plantasForm = !estado;
+    this.departamentosForm = !estado;
+    this.parteForm = !estado;
+    this.funcionForm = !estado;
+  }
+  catalogoPlantas(estado:boolean){
+    this.datoGeneral   = !estado;
+    this.maquinasForm = !estado;
+    this.plantasForm = estado;
+    this.departamentosForm = !estado;
+    this.parteForm = !estado;
+    this.funcionForm = !estado;
+  }
+  catalogoDepartamentos(estado:boolean){
+    this.datoGeneral   = !estado;
+    this.maquinasForm = !estado;
+    this.plantasForm = !estado;
+    this.departamentosForm = estado;
+    this.parteForm = !estado;
+    this.funcionForm = !estado;
+  }
+  catalogoPartes(estado:boolean){
+    this.datoGeneral   = !estado;
+    this.maquinasForm = !estado;
+    this.plantasForm = !estado;
+    this.departamentosForm = !estado;
+    this.parteForm = estado;
+    this.funcionForm = !estado;
+  }
+  catalogoFunciones(estado:boolean){
+    this.datoGeneral   = !estado;
+    this.maquinasForm = !estado;
+    this.plantasForm = !estado;
+    this.departamentosForm = !estado;
+    this.parteForm = !estado;
+    this.funcionForm = estado;
+  }
+
 }
